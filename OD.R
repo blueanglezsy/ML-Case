041dc15e5539f52a762438cd5c1c639891962c32
Shiyun <syzhang@knights.ucf.edu>
2017-03-14 22:42:52 -0500

    Update OD.R

--- OD.R
+++ OD.R
@@ -4,13 +4,14 @@ library(hexbin)
 
 # Read data
 untar("C:/Users/syzha/Desktop/New folder/OpenDoor/knn_data.tar.gz",list=TRUE) 
-data = untar("C:/Users/TKN005L/Desktop/DS/R Case/OD/knn_data.tar.gz")
+data = untar("C:/Users/syzha/Desktop/New folder/OpenDoor/knn_data.tar.gz")
 data = read.csv("data.csv")
 data = data[order(data$close_date),]
-test = data[1:100,]
-test = test[order(test$close_date),]
+l = nrow(data)
+train = data[1:70000,]
+test = data[70001:l,]
 
-# KNN Implementation 1: temporal prediction
+# KNN Implementation 1: Temporal Prediction adaptive (All records)
 knn <- function(df, k){
   df = df[order(df$close_date),]
   n <- nrow(df)
@@ -21,7 +22,7 @@ knn <- function(df, k){
     euc.dist <- colSums((as.numeric(df[i, 1:2]) - t(as.matrix(df[1:i-1, 1:2]))) ^ 2)  
     neigh[i, ] <- order(euc.dist)[1:k]
     dsq <- euc.dist[order(euc.dist)[1:k]]
-    weight <- 1/dsq
+    weight <- ifelse(dsq == 0, .Machine$integer.max/k, 1/dsq)
     predictions[i] <- sum(weight*df$close_price[neigh[i, ]])/sum(weight)
   }
   return (predictions)
@@ -37,25 +38,81 @@ data_knn1 <- transform(data, KNNPredictions = knn(data, 4))
 
 # Evaluate the overall performance
 MRAE(data_knn1)
+summary(data_knn1)
+data_knn1[data_knn1$KNNPredictions == Inf,]
 
 # Looking into the error trend by time
 data_knn1$RAE <- abs(data_knn1$KNNPredictions - data_knn1$close_price)/data_knn1$close_price
-data_knn1$close_date <= as.Date(data_knn1$close_date)
+data_knn1$close_date = as.numeric(data_knn1$close_date)
 ggplot(data = data_knn1, aes(x = close_date, y = RAE)) +
   geom_line()
 
 # Looking into the error trend by spatial
-ggplot(data = data_knn1, aes(latitude, longitude)) +
-  stat_summary_hex(fun = function(x) median(x))
-  
+data_knn1 = data_knn1[-c(1:4),]
+ggplot(data = data_knn1, aes(latitude, longitude, z = RAE)) +
+  stat_summary_hex (fun = function(x) median(x))
+
+# KNN Implementation 2: Temporal Prediction for test dataset
+knn <- function(df_train, df_test, k){
+  n_train <- nrow(df_train)
+  n_test <- nrow(df_test)
+  if (n_train <= k) stop("k can not be more than n-1")
+  neigh <- matrix(0, nrow = n_test, ncol = k)
+  predictions <- rep(NA, n_test)
+  for(i in 1:n_test) {
+    euc.dist <- colSums((as.numeric(df_test[i, 1:2]) - t(as.matrix(df_train[1:n_train, 1:2]))) ^ 2)  
+    neigh[i, ] <- order(euc.dist)[1:k]
+    dsq <- euc.dist[order(euc.dist)[1:k]]
+    weight <- ifelse(dsq == 0, .Machine$integer.max/k, 1/dsq)
+    predictions[i] <- sum(weight*df_train$close_price[neigh[i, ]])/sum(weight)
+  }
+  return (predictions)
+}
+
+MRAE <- function(df){
+  MRAE <- median(abs(df$KNNPredictions - df$close_price)/df$close_price, na.rm = TRUE)
+  return(MRAE)
+}
+
+# Make the predictions
+data_knn2 <- transform(test, KNNPredictions = knn(train, test, 4))
+
+# Evaluate the overall performance
+MRAE(data_knn2)
+summary(data_knn2)
+data_knn2[data_knn2$KNNPredictions == Inf,]
+
+# Looking into the error trend by time
+data_knn2$RAE <- abs(data_knn2$KNNPredictions - data_knn2$close_price)/data_knn2$close_price
+data_knn2$close_date = as.numeric(data_knn2$close_date)
+ggplot(data = data_knn2, aes(x = close_date, y = RAE)) +
+  geom_line()
+
+# Looking into the error trend by spatial
+ggplot(data = data_knn2, aes(latitude, longitude, z = RAE)) +
+  stat_summary_hex (fun = function(x) median(x))
+
+# Optimize K
+best_k = rep(NA, 10)
+for (k in 1:10){
+  pred <- transform(test, KNNPredictions = knn(train, test, 4))
+  best_k[k] = MRAE(pred)
+}
+
+
+
+# KNN Implementation for random sampling setting? 
+# (I don't think in our case we should use this one cause our data come into in a temporal manner)
 # Select the optimal k by CV
+fold <- sample(1:5,75,replace=TRUE)
+cvpred <- matrix(NA,nrow=75,ncol=10)
+for (k in 1:10){
+  for (i in 1:5){
+    # cross validation get the error rate plotted by k 
+  }
+}
 
 
-
-
-
-
-# KNN Implementation 2: predict test based on previous records
 
 
 
